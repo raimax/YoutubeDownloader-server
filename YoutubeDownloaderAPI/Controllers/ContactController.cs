abb7f48deeb38337397079f52dea0d428ae55f82
raimax <4miner44@gmail.com>
2021-07-30 11:05:54 +0300

    v1.0

---
+++ YoutubeDownloaderAPI/Controllers/ContactController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using YoutubeDownloaderAPI.Models;
+
+namespace YoutubeDownloaderAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactController : ControllerBase
+    {
+        private readonly Contact _contact;
+
+        public ContactController()
+        {
+            _contact = new Contact();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Contact(Contact contact)
+        {
+            _contact.Name = contact.Name;
+            _contact.Message = contact.Message;
+
+            if (String.IsNullOrWhiteSpace(contact.Email))
+            {
+                _contact.Email = "None";
+            }
+            else
+            {
+                _contact.Email = contact.Email;
+            }
+
+            Mail mail = new Mail(_contact.Name, _contact.Email, _contact.Message);
+
+            try
+            {
+                await mail.SendMail();
+                return Ok(new { message = "Message sent successfully" });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+    }
+}
---
+++ YoutubeDownloaderAPI/Models/Mail.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace YoutubeDownloaderAPI.Models
+{
+    public class Mail
+    {
+        private string From { get; set; } = "ytdl.server@gmail.com";
+        private string To { get; set; } = "4miner44@gmail.com";
+        private string ServerMailPassword { get; set; } = "S4Au5[*S)3'&_u&?";
+        public string Body { get; set; }
+        public string SenderName { get; set; }
+        public string SenderEmail { get; set; }
+
+        public Mail(string name, string email, string body)
+        {
+            SenderName = name;
+            SenderEmail = email;
+            Body = body;
+        }
+
+        public async Task SendMail()
+        {
+            MailMessage mail = new MailMessage();
+            mail.To.Add(To);
+            mail.From = new MailAddress(From);
+            mail.Subject = "New message from server " + DateTime.Now.ToString();
+            mail.Body = $"Name: {SenderName}<br> Email: {SenderEmail}<br><br> {Body}";
+            mail.IsBodyHtml = true;
+
+            SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
+            smtp.Credentials = new System.Net.NetworkCredential(From, ServerMailPassword);
+            smtp.EnableSsl = true;
+
+            await smtp.SendMailAsync(mail);
+        }
+    }
+}
